e94343194d672f9d2879385ab0f7ac2938b56ea1
SBDJAY <sbdjay183@gmail.com>
2023-04-09 18:01:58 -0400

    Updated

---
+++ TempFiles.py
@@ -0,0 +1,37 @@
+'''This is code for error handeling for numOfsensors in createsensors def and the id name variable that goes in the list'''
+
+
+
+# listSensors = []
+    
+#     def createSensors():
+#         numOfSensors = int(input('Enter the number of sensors: '))  #Takes the integer input for the Number of sensors
+#         index = 0
+#         #while numOfSensors == True:     #While Loop to check if numOfSensors is an int value (If true then will ask for the Sensor ID if false it will return the question)
+#             #if numOfSensors == int:
+#                 #------------------------------------------------------------------------
+#         while index < numOfSensors:     #While loop to check if Wireless.id from WirelessNetwork Class(Module) is an Integer value (if true then appends listSensors from the Application Class, and a)
+#             WirelessNetwork.id = str(input("Enter the Sensor ID: "))
+#                     #while WirelessNetwork.id == True:
+#                         #if WirelessNetwork.id == str:
+#             Application.listSensors.append (WirelessNetwork.id)
+#             index += 1
+#                 #else:
+#                     #print('This is an invalid entry for sensor Id!')
+#                 #-------------------------------------------------------------------------
+#             #else:
+#                 #print('This is an invalid entry for the number of sensors!')
+        
+#         print(Application.listSensors)
+
+# x=int(input("enter the size of list"))
+# y= int(input("enter the size of sublist"))
+# list1=[]
+# sublist=[]
+# for i in range(x):
+#     for j in range(y):
+#         sublist.append(input())
+#     list1.append(sublist)
+#     sublist=[]
+# print (list1)
+
---
+++ Wireless_Networks_Module.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------
+#Daniel Pius
+#Tarek El Salti
+#April 9 2023
+#PROG 10004 PROGRAMING PRINCIPLES
+#Wireless Networks Module
+#-------------------------------------------------
+#Import
+import time
+#-------------------------------------------------
+class WirelessNetwork():
+    #constants
+    BRAND_NAME = "Cisco brand" 
+    ADHOC_Mode = "ON"
+    #Attributes
+    id = str
+    oxygenLevel = int
+    temperature = float
+    link = []
+
+    def greetMessage():      
+        #This Definiition prints out a welcome message and is only called once, contains constants BRAND_NAME and ADHOC_MODE
+        print('******************************************************************************')
+        print("Welcome to the company's IoT-Based Health System")
+        time.sleep(3)
+        print('These are sensors of brand',WirelessNetwork.BRAND_NAME,'and their Ad Hoc Mode is',WirelessNetwork.ADHOC_Mode)
+        print('******************************************************************************') 
